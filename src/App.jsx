8116058cc77be915efaf73a842b406a903f32cd9
navinkumar-classic <navinkumar.ao2022@vitstudent.ac.in>
2025-11-17 14:30:36 +0530

    routing implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
 import PomodoroSection from "./assets/PomodoroSection.jsx";
 import PDFViewer from "./assets/PDFViewer.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-
+function PDFPage() {
 
     return (
         <>
@@ -15,5 +15,30 @@ function App() {
         </>
     );
 }
+
+function PomodoroPage() {
+
+    return (
+        <>
+            <div className="stars"></div>
+            <div className="twinkling"></div>
+            <div className="clouds"></div>
+
+            <PomodoroSection />
+        </>
+    );
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<PomodoroPage />} />
+                <Route path="/pdf-viewer" element={<PDFPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
 
 export default App;
